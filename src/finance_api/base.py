40914fc6f9d0db4d7dd27e882600acb17ff868fb
Tobias Ahlers <93325697+TobiasAhlers@users.noreply.github.com>
2025-02-24 16:28:02 +0100

    Add AccountRouter with balance retrieval endpoint to FinanceAPI

--- src/finance_api/base.py
+++ src/finance_api/base.py
@@ -5,6 +5,7 @@ from data_model_router import DataModelRouter
 from sqlmodel import SQLModel
 
 from .database import *
+from .router import *
 
 
 class FinanceAPI(FastAPI):
@@ -17,7 +18,7 @@ class FinanceAPI(FastAPI):
         SQLModel.metadata.create_all(ENGINE, checkfirst=True)
 
         self.include_router(DataModelRouter(Transaction))
-        self.include_router(DataModelRouter(Account))
+        self.include_router(AccountRouter())
         self.include_router(DataModelRouter(Category))
 
         self.add_middleware(
